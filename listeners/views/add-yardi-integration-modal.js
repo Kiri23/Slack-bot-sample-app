375b0ec61a5a0d706ba093a1130011aa58762176
Christian Nogueras <christian@opticpower.com>
2022-01-17 04:31:34 +0000

    send form values to callback

--- listeners/views/add-yardi-integration-modal.js
+++ listeners/views/add-yardi-integration-modal.js
@@ -6,41 +6,46 @@ const addYardiIntegrationModalCallback = async ({ ack, view, body, client }) => 
   const providedValues = view.state.values;
   console.log('form values:', providedValues);
 
+  const wsdl = providedValues.yardiWsdl.yardiWsdl.value;
+  console.log('wsdl',wsdl );
   const soapUrl = providedValues.yardiSoap.yardiSoap.value;
-  console.log('soap url',soapUrl );
-  const selectedDate = providedValues.taskDueDate.taskDueDate.selected_date;
-  const selectedTime = providedValues.taskDueTime.taskDueTime.selected_time;
+  const username = providedValues.yardiUserName.yardiUserName.value;
+  const password = providedValues.yardiPassword.yardiPassword.value;
+  const alias = providedValues.yardiAlias.yardiAlias.value;
 
-  if (selectedDate) {
-    if (!selectedTime) {
+  const activationDate = providedValues.yardiActivationDate.yardiActivationDate.selected_date;
+  const activationTime = providedValues.yardiActivationTime.yardiActivationTime.selected_time;
+
+  if (activationDate) {
+    if (!activationTime) {
       await ack({
         response_action: 'errors',
         errors: {
-          taskDueTime: "Please set a time for the date you've chosen",
+          yardiActivationDate: "Please set a time for the date you've chosen",
         },
       });
       return;
     }
-    const taskDueDate = DateTime.fromISO(`${selectedDate}T${selectedTime}`);
+    const taskDueDate = DateTime.fromISO(`${activationDate}T${activationTime}`);
     const diffInDays = taskDueDate.diffNow('days').toObject().days;
     // Task due date is in the past, so reject
     if (diffInDays < 0) {
       await ack({
         response_action: 'errors',
         errors: {
-          taskDueDate: 'Please select a due date in the future',
-          taskDueTime: 'Please select a time in the future',
+          yardiActivationDate: 'Please select a due date in the future',
+          yardiActivationTime: 'Please select a time in the future',
         },
       });
       return;
     }
-    task.dueDate = taskDueDate;
   }
 
+  // TODO: Save form values to DB
   try {
     await ack({
       response_action: 'update',
-      view: modals.taskCreated('my titutlo espeecifico'),
+      view: modals.taskCreated(providedValues),
     });
   } catch (error) {
     await ack({
--- user-interface/modals/add-yardi-integration.js
+++ user-interface/modals/add-yardi-integration.js
@@ -1,31 +1,31 @@
 const { Modal, Blocks, Elements } = require('slack-block-builder');
 
 module.exports = () => {
-  return Modal({ title: 'Add a new yardi integration', submit: 'Create', callbackId: 'add-yardi-integration' })
+  return Modal({ title: 'Create yardi integration', submit: 'Create', callbackId: 'add-yardi-integration' })
     .blocks(
       Blocks.Input({ label: 'WSDL Url', blockId: 'yardiWsdl' }).element(
-        Elements.TextInput()
+        Elements.TextInput({actionId:'yardiWsdl'})
       ),
       Blocks.Input({ label: 'Soap endpoint', blockId: 'yardiSoap' }).element(
-        Elements.TextInput()
+        Elements.TextInput({actionId:'yardiSoap'})
       ),
       Blocks.Input({ label: 'Username', blockId: 'yardiUserName' }).element(
-        Elements.TextInput()
+        Elements.TextInput({actionId:'yardiUserName'})
       ),
       Blocks.Input({ label: 'Password', blockId: 'yardiPassword' }).element(
-        Elements.TextInput()
+        Elements.TextInput({actionId:'yardiPassword'})
       ),
       Blocks.Input({ label: 'Alias', blockId: 'yardiAlias' }).element(
-        Elements.TextInput()
+        Elements.TextInput({actionId:'yardiAlias'})
       ),
-      Blocks.Input({ label: 'Due date', blockId: 'taskDueDate', optional: true }).element(
+      Blocks.Input({ label: 'Due date', blockId: 'yardiActivationDate', optional: true }).element(
         Elements.DatePicker({
-          actionId: 'taskDueDate',
+          actionId: 'yardiActivationDate',
         }),
       ),
-      Blocks.Input({ label: 'Time', blockId: 'taskDueTime', optional: true }).element(
+      Blocks.Input({ label: 'Time', blockId: 'yardiActivationTime', optional: true }).element(
         Elements.TimePicker({
-          actionId: 'taskDueTime',
+          actionId: 'yardiActivationTime',
         }),
       ),
     ).buildToJSON();
